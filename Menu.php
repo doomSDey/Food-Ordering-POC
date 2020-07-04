113698d34be631a5b9a27bf8f2e955e23f06b411
doomSDey <sdptd20@gmail.com>
2020-07-03 17:56:10 -0700

    menu

---
+++ Menu.php
@@ -0,0 +1,44 @@
+<DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="node_modules/bootstrap-social/bootstrap-social.css">
+    <link rel="stylesheet" href="css/stylesmenu.css" type="text/css" rel="stylesheet">
+
+    <title>ConFusion</title>
+  </head>
+
+  <body class="bg">
+    <nav class="navbar navbar-expand-sm fixed-top ">
+
+      <!-- Links -->
+      <ul class="navbar-nav">
+        <a class="navbar-brand logo" href="#">conFUSION</a>
+        <form class="form-inline" action="/action_page.php">
+            <input class="form-control mr-sm-2 search" type="text" placeholder="Search">
+            <button class="btn " type="submit">Search</button>
+        </form>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cart</a>
+        </li>
+      </ul>
+
+    </nav>
+    <div>
+    </div>
+    <div class="container">
+
+    </div>
+    <script src="jquery/jquery-3.5.1.min.js" type="text/javascript"></script>
+    <script src="bootstrap-4.3.1-dist/js/bootstrap.bundle.js" type="text/javascript"></script>
+  </body>
+
+  </html>
